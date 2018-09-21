0d8129326869176b1ebae75557afeeb193ee440d
t-kusakabe <ttmac0704@gmail.com>
2018-09-21 09:07:42 +0900

    Add basic_types.exs

--- lib/basic_types.exs
+++ lib/basic_types.exs
@@ -6,3 +6,11 @@ IO.puts :atom
 IO.puts 'elixir'
 IO.inspect [1 ,2, 3]
 IO.inspect {1, 2, 3}
+
+IO.puts 1 + 2
+IO.puts 5 * 5
+IO.puts 10 / 2
+
+IO.puts div(10, 2)
+IO.puts div 10, 2
+IO.puts rem 10, 3
