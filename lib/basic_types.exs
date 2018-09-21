99e1a4ff1efbc1da0ed0a9c0344087f0f1f0b927
t-kusakabe <ttmac0704@gmail.com>
2018-09-21 09:19:12 +0900

    Add basic_types.exs

--- lib/basic_types.exs
+++ lib/basic_types.exs
@@ -3,7 +3,7 @@ IO.puts 0x1F
 IO.puts 1.0
 IO.puts true
 IO.puts :atom
-IO.puts 'elixir'
+IO.puts "elixir"
 IO.inspect [1 ,2, 3]
 IO.inspect {1, 2, 3}
 
@@ -25,3 +25,26 @@ IO.puts 1.0e-10
 IO.puts round 3.58
 IO.puts round 3.49
 IO.puts trunc 3.58
+
+IO.puts true
+IO.puts true == false
+
+IO.puts is_boolean true
+IO.puts is_boolean 1
+
+IO.puts :hello
+IO.puts :hello == :world
+
+IO.puts true == :true
+IO.puts is_atom false
+
+IO.puts "hello"
+IO.puts "hello #{:world}"
+
+IO.puts is_binary "hello"
+IO.puts byte_size "hellö"
+IO.puts String.length "hellö"
+
+IO.puts String.upcase "hello"
+
+IO.puts 'hello' == "hello"
